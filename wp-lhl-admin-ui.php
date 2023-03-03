5db2d5f8d2e7887aef0b18a417181af64bb1b0be
Lehel Matyus <lehel@urbaninsight.com>
2023-03-02 23:48:32 -0600

    Compat for style adding

--- wp-lhl-admin-ui.php
+++ wp-lhl-admin-ui.php
@@ -25,7 +25,9 @@ if (!class_exists('WpLHLAdminSettingsLink')) {
 /**
  * Enq Admin Styles
  */
-function wp_lhl_admin_ui_styles() {
-    wp_enqueue_style('wp-lhl-admin-ui-styles',  plugin_dir_url( dirname( __FILE__ ) ) . 'wp-lhl-admin-ui/css/wp-lhl-admin-ui.css');
+if( !function_exists('wp_lhl_admin_ui_styles')) { 
+    function wp_lhl_admin_ui_styles() {
+        wp_enqueue_style('wp-lhl-admin-ui-styles',  plugin_dir_url( dirname( __FILE__ ) ) . 'wp-lhl-admin-ui/css/wp-lhl-admin-ui.css');
+    }
+    add_action('admin_enqueue_scripts', 'wp_lhl_admin_ui_styles');
 }
-add_action('admin_enqueue_scripts', 'wp_lhl_admin_ui_styles');
