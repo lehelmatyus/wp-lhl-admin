9294c72038800844e08e66eaf5742b27d8f00aef
Lehel Matyus <lehel@urbaninsight.com>
2023-03-26 00:57:16 -0500

    Fix path to Css

--- src/WpLHLAdminUi.php
+++ src/WpLHLAdminUi.php
@@ -6,7 +6,7 @@ class WpLHLAdminUi{
      * Enq Admin Styles
      */
     function wp_enqueue_style() {
-        wp_enqueue_style('wp-lhl-admin-ui-styles',  plugin_dir_url( dirname( __FILE__ ) ) . '/vendor/lehelmatyus/wp-lhl-admin-ui/css/wp-lhl-admin-ui.css');
+        wp_enqueue_style('wp-lhl-admin-ui-styles',  plugin_dir_url( dirname( __FILE__ ) ) . '/vendor/lehelmatyus/css/wp-lhl-admin-ui.css');
     }
 
 }
