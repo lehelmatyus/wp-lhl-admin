3454754abe4ab8fbd0ba9e1a0e5d968f6e70c3ea
Lehel Matyus <lehel@urbaninsight.com>
2023-08-15 02:59:53 -0500

    Refine buttons add Email template handler.

--- example/ajaxButtons/ajaxButtons.js
+++ example/ajaxButtons/ajaxButtons.js
@@ -1,5 +1,5 @@
-(function( $ ) {
-	'use strict';
+(function ($) {
+	"use strict";
 
 	/**
 	 * All of the code for your admin-facing JavaScript source
@@ -29,55 +29,95 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
-	$(function() {
+	$(function () {});
+})(jQuery);
 
-	});
-
-})( jQuery );
-
-
-function ajaxButtonExample(e){
-
-	e.preventDefault();	
+function ajaxButtonExample(e) {
+	e.preventDefault();
 	let id = e.target.id;
 	let btn = e.target;
-	let msg = document.querySelector('.'+id+'__msg');
-	let userId = btn.dataset.userId;
-	console.log("userdata being sent");
-	console.log(userId);
+	let msg = document.querySelector("." + id + "__msg");
 
-	
-	jQuery.ajax({
-		url: tpulApiSettings.root + 'terms-popup-on-user-login/v1/action/reset-single-user',
-		type: 'POST',
-		contentType: 'application/json',
-		beforeSend: function (xhr) {
-			xhr.setRequestHeader('X-WP-Nonce', tpulApiSettings.tpul_nonce);
-			btn.disabled = true;
-			msg.innerHTML = msg.dataset.waitMsg;
-		},
-		data: JSON.stringify( {
-			userId: userId,
+	jQuery
+		.ajax({
+			url: bbrcApiSettings.root + "bb-report-content/report/comment",
+			type: "POST",
+			contentType: "application/json",
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("X-WP-Nonce", bbrcApiSettings.nonce);
+				btn.disabled = true;
+				msg.innerHTML = msg.dataset.waitMsg;
+			},
+			data: JSON.stringify({
+				comment_id: "123",
+			}),
+			success: function (response) {},
 		})
-		,
-		success: function (response){
-		}
+		.done(function (results) {
+			btn.disabled = false;
+			msg.innerHTML = msg.dataset.successMsg;
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			let message = "no response";
+			if (jqXHR.responseJSON) {
+				message = jqXHR.responseJSON.message;
+			}
+			btn.disabled = false;
+			msg.innerHTML =
+				"Script Failed. Error: " + errorThrown + " Message: " + message;
+		});
+}
 
-	}).done(function (results) {
+function bbrc_report_click(e) {
+	e.preventDefault();
 
-		console.log( results );
-		
-		btn.disabled = false;
-		msg.innerHTML = msg.dataset.successMsg;
+	// Get the current page URL
+	const currentPageURL = window.location.href;
+	// Remove hash values from the URL
+	const urlWithoutHash = currentPageURL.split("#")[0];
 
-	}).fail(function (jqXHR, textStatus, errorThrown) {
-		
-		console.log(jqXHR);
-		console.log(textStatus);
-		console.log(errorThrown);
+	let id = e.target.id;
+	let btn = e.target;
+	let dataset = e.target.dataset.vals;
+	let msg = document.querySelector("." + id + "__msg");
 
-		btn.disabled = false;
-		msg.innerHTML = 'Script Failed. Error: ' + errorThrown + " " + jqXHR.responseJSON.message;
+	jQuery
+		.ajax({
+			url: bbrcApiSettings.root + "bb-report-content/report/comment",
+			type: "POST",
+			contentType: "application/json",
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("X-WP-Nonce", bbrcApiSettings.nonce);
+				btn.disabled = true;
+				// btn.classList.add("hidden");
 
-	});
+				msg.innerHTML = msg.dataset.waitMsg;
+				msg.classList.remove("hidden");
+				msg.classList.remove("msg-success");
+				msg.classList.remove("msg-error");
+				msg.classList.add("msg-waiting");
+			},
+			data: JSON.stringify({
+				data: dataset,
+				pageUrl: urlWithoutHash,
+			}),
+			success: function (response) {
+				msg.classList.add("msg-success");
+				msg.innerHTML = msg.dataset.successMsg;
+			},
+		})
+		.done(function (results) {
+			// btn.disabled = false;
+			msg.classList.remove("msg-waiting");
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			msg.classList.add("msg-error");
+			let message = "no response";
+			if (jqXHR.responseJSON) {
+				message = jqXHR.responseJSON.message;
+			}
+			btn.disabled = false;
+			msg.innerHTML =
+				"Script Failed. Error: " + errorThrown + " Message: " + message;
+		});
 }
