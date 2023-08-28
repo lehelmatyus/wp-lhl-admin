bb2ba0c2b876e2cb7c8aa79dd3e8d15d240bad66
Lehel Matyus <lehel@urbaninsight.com>
2023-08-28 16:30:22 -0500

    Update readme.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Helper library to create plugin and theme settings and options page form element
 
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+git tag 1.0.x
+git push origin --tags
--- src/WpLHLAdminUi.php
+++ src/WpLHLAdminUi.php
@@ -1,11 +1,10 @@
 <?php
 
-class WpLHLAdminUi{
+class WpLHLAdminUi {
 
     private $uniqKey = "";
 
-    function __construct($uniqKey)
-    {
+    function __construct($uniqKey = '') {
         $this->uniqKey = $uniqKey;
     }
 
@@ -14,10 +13,9 @@ class WpLHLAdminUi{
      */
     function wp_enqueue_style() {
         $uniqKey = "";
-        if(!empty($this->uniqKey)){
+        if (!empty($this->uniqKey)) {
             $uniqKey = " " . $this->uniqKey;
         }
-        wp_enqueue_style('wp-lhl-admin-ui-styles' . esc_attr($uniqKey),  plugin_dir_url( dirname( __FILE__ ) ) . '/css/wp-lhl-admin-ui.css');
+        wp_enqueue_style('wp-lhl-admin-ui-styles' . esc_attr($uniqKey),  plugin_dir_url(dirname(__FILE__)) . '/css/wp-lhl-admin-ui.css');
     }
-
 }
